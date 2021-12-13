5d898549b1d8ddc70ea69b7d3f488d55e0173421
Yonatan Karp-Rudin <yonatan.karp@sumup.com>
2021-12-13 12:37:13 +0100

    Implement the bot logic

--- src/main/kotlin/com/example/TwitterBot.kt
+++ src/main/kotlin/com/example/TwitterBot.kt
@@ -1,8 +1,62 @@
 package com.example
 
+import twitter4j.Status
+import twitter4j.TwitterFactory
+
 object TwitterBot {
     @JvmStatic
     fun main(args: Array<String>) {
-        println("Twitter Bot Starter is running!")
+        tweetAll()
     }
+
+    /**
+     * Reads all tweets from `tweets.txt` file under resources
+     * directory and tweet them one by one.
+     * with a delay between them
+     */
+    private fun tweetAll() {
+        val tweets = getResourceAsLines("tweets.txt")
+        tweets.forEach { tweet ->
+            sendTweet(tweet)
+            println("Tweeting: $tweet...")
+            waitUntilNextTweet()
+        }
+    }
+
+    /**
+     * Reads the content of a given file.
+     *
+     * @param resource the path under the resources directory
+     *        to read from
+     * @return list of strings representing each line in the file.
+     */
+    private fun getResourceAsLines(resource: String): List<String> =
+        TwitterBot.javaClass.classLoader
+            .getResource(resource)
+            .readText()
+            .lines()
+
+    /**
+     * Publishing a given tweet into the Twitter account.
+     */
+    private fun sendTweet(tweet: String) = kotlin.runCatching {
+        TwitterFactory.getSingleton().updateStatus(tweet)
+    }
+        .onSuccess { status: Status -> println(status) }
+        .onFailure { it.printStackTrace() }
+
+    /**
+     * Waits for a constant amount of time between each every 2
+     * tweets we want to publish.
+     *
+     * @param delayInSeconds the number of minutes to delay,
+     *  30min by default.
+     */
+    private fun waitUntilNextTweet(delayInSeconds: Long = 30) =
+        kotlin.runCatching {
+            println("Sleeping for $delayInSeconds minutes...")
+            // Change here to increase or decrease the delay
+            Thread.sleep(delayInSeconds * 60000)
+        }
+            .onFailure { it.printStackTrace() }
 }
